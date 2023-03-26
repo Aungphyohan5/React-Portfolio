ed3a5efdf52d2eb214911abe71b70382cef6dfa6
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-03-26 11:00:47 -0300

    added styling for contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import '../../styles/Contact.css';
 
 
 export default function Contact() {
@@ -16,21 +17,32 @@ export default function Contact() {
       't-dfFqeX4KEVQG46U')
       .then((result) => {
         console.log(result.text);
-        console.log
       }, (error) => {
         console.log(error.text);
       });
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className='contact-bg vh-100 text-white'>
+      <form ref={form} onSubmit={sendEmail} className='mx-auto col-10 col-md-8 col-lg-6 form-container'>
+        <h1>Contact</h1>
+        <div className='form-group mt-4'>
+          <label>Name</label>
+          <input type="text" name="user_name" className='form-control' />
+        </div>
+
+        <div className='form-group mt-4'>
+          <label>Email</label>
+          <input type="email" name="user_email" className='form-control' />
+        </div>
+
+        <div className='form-group mt-4'>
+          <label>Message</label>
+          <textarea name="message" className='form-control textbox-height' />
+        </div>
+        <input type="submit" value="Send" className='btn btn-warning mt-4' />
+      </form>
+    </div>
+
   );
 }
