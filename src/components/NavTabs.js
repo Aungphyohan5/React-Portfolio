766f512ad9dd9de811b9f19cead84a551141818b
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-03-27 16:00:00 -0300

    navbar display moved to right

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+
+
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs border-0" style={{ background: '#000000' }}>
+    <ul className="nav nav-tabs border-0 d-flex justify-content-end" style={{ background: '#000000' }}>
       <li className="nav-item">
         <a
           href="#about"
