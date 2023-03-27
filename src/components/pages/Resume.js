769e64f4d2bbbdada2be02aef04bd98d39991dc3
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-03-27 16:09:28 -0300

    added resume page & styling

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,21 +1,37 @@
 import React from 'react';
+import '../../styles/Resume.css';
 
 export default function Resume() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    <div className='resume-bg'>
+      <div className='p-5'>
+        <p>Please download the resume<a href="" download>
+          Click to download
+        </a></p>
+        <h3>Front-end Proficiencies</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>JQuery</li>
+          <li>responsive design</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+          <li>Bulma</li>
+        </ul>
+        <h3>Back-end Proficiencies</h3>
+        <ul>
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MySQL, Sequelize</li>
+          <li>MongoDB, Mongoose</li>
+          <li>REST</li>
+          <li>GraphQL</li>
+        </ul>
+      </div>
     </div>
+
+
   );
 }
