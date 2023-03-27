849251271a445493e157dfc3e9540bbfdb8bc453
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-03-27 17:58:22 -0300

    added name in navbar

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -6,48 +6,59 @@ import React from 'react';
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs border-0 d-flex justify-content-end" style={{ background: '#000000' }}>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#Portfolio"
-          onClick={() => handlePageChange('Portfolio')}
-          // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-        >
-          Portfolio
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#Resume"
-          onClick={() => handlePageChange('Resume')}
-          // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </a>
-      </li>
-    </ul>
+    <div className='d-flex justify-content-md-between' style={{ background: '#000000' }}>
+      <div>
+        <h1 style={{ color: 'orange' }}>𝑨𝒖𝒏𝒈</h1>
+      </div>
+      <div>
+        <ul className='nav nav-tabs border-0 d-flex justify-content-end'>
+
+          <li className="nav-item">
+            <a
+              href="#about"
+              onClick={() => handlePageChange('About')}
+              // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+              className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+            >
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#Portfolio"
+              onClick={() => handlePageChange('Portfolio')}
+              // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+              className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+            >
+              Portfolio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#contact"
+              onClick={() => handlePageChange('Contact')}
+              // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+            >
+              Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#Resume"
+              onClick={() => handlePageChange('Resume')}
+              // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+              className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
+
+
+    </div>
+
   );
 }
 
